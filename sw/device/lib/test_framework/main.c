72da0290dbd17afb53c6f0e0f281b1302d0020fd
Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>
2026-01-22 14:55:20 +0000

    sw: test framework: split off exiting test into test_exit, refactor
    Signed-off-by: Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>

--- sw/device/lib/test_framework/main.c
+++ sw/device/lib/test_framework/main.c
@@ -8,6 +8,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/* magic byte string to terminate the verilator simulation */
+static const char magic[] = "\xd8\xaf\xfb\xa0\xc7\xe1\xa9\xd7";
+
 /* the test's main function. returns whether the test was successful or not */
 [[gnu::weak]] bool test_main(uart_t console)
 {
@@ -16,23 +19,33 @@
     return false;
 }
 
-int main(void)
+/* exit the test with a pass or fail */
+[[noreturn]] void test_exit(bool success)
+{
+    uart_t console = mocha_system_uart();
+    uart_puts(console, "TEST RESULT: ");
+    if (success) {
+        uart_puts(console, "PASSED");
+    } else {
+        uart_puts(console, "FAILED");
+    }
+    uart_putchar(console, '\n');
+    uart_puts(console, "Safe to exit simulator.");
+    uart_puts(console, magic);
+
+    /* magic string should have terminated the verilator simulation */
+    while (true) {
+    }
+}
+
+[[noreturn]] void main(void)
 {
     uart_t console = mocha_system_uart();
     uart_init(console);
 
     bool result = test_main(console);
-    uart_puts(console, "TEST RESULT: ");
-    if (result) {
-        uart_puts(console, "PASSED");
-    } else {
-        uart_puts(console, "FAILED");
-    }
 
-    // This will kill the simulation if we are running on verilator.
-    uart_puts(console, "\nSafe to exit simulator.\xd8\xaf\xfb\xa0\xc7\xe1\xa9\xd7");
-
-    return 0;
+    test_exit(result);
 }
 
 void _trap_handler(struct trap_registers *registers, struct trap_context *context)
