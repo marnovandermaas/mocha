c09240a7ee0f6fbc0d5df8658fe0398967637a84
Marno van der Maas <mvdmaas+git@lowrisc.org>
2026-03-16 13:22:33 +0000

    [sw] SPI host smoketest stub

---
+++ sw/device/tests/spi_host/smoketest.c
@@ -0,0 +1,12 @@
+// Copyright lowRISC contributors (COSMIC project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "hal/mocha.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+bool test_main()
+{
+    return true;
+}
