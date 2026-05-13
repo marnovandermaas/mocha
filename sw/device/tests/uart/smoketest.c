c91c44c38c77a85dea39c60c5ba2fa9968bb5295
Douglas Reis <doreis@lowrisc.org>
2026-05-13 11:05:06 +0000

    [sw,uart] Refactor smoketest
    The test was sensitive to timing, when optimization was enabled it would
    fails as the loop back transmission would not have finished in time for
    checking.
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/tests/uart/smoketest.c
+++ sw/device/tests/uart/smoketest.c
@@ -3,26 +3,36 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "hal/uart.h"
+#include "runtime/print.h"
 #include <stdbool.h>
 #include <stdint.h>
 
-const char uart_loopback_test_string[] = "Test String";
+const static char uart_loopback_test_string[] = "Test String";
 
 static bool loopback_test(uart_t uart)
 {
+    // Flush the uart
+    uart_wait_for(uart, uart_status_txidle);
+
     uart_loopback_set(uart, true, true);
     for (uint32_t idx = 0; idx < sizeof(uart_loopback_test_string); idx++) {
         uart_out(uart, uart_loopback_test_string[idx]);
     }
+    // Wait for the transmission to finish
+    uart_wait_for(uart, uart_status_txidle);
+    uart_loopback_set(uart, false, false);
 
     bool res = true;
     for (uint32_t idx = 0; idx < sizeof(uart_loopback_test_string); idx++) {
-        if (uart_in(uart) != uart_loopback_test_string[idx]) {
+        while (uart_status_any(uart, uart_status_rxempty)) {
+        };
+        char rx = uart_in(uart);
+        if (rx != uart_loopback_test_string[idx]) {
+            uprintf(uart, "Expected: %c, got: %c\n", rx, uart_loopback_test_string[idx]);
             res = false;
             break;
         }
     }
-    uart_loopback_set(uart, false, false);
     return res;
 }
 
