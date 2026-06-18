0f18d3680a917e75f19cb7eb6cd2a7880a96e3b5
Marno van der Maas <mvdmaas+git@lowrisc.org>
2026-06-18 16:23:39 +0100

    DO NOT MERGE: test GDB command

--- util/debug_test.sh
+++ util/debug_test.sh
@@ -4,10 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 build/lowrisc_mocha_top_chip_verilator_0/sim-verilator/Vtop_chip_verilator -r build/sw/device/bootrom/bootrom_scrambled.vmem -E build/sw/device/examples/infinite_loop &
-sleep 20
+sleep 5
 openocd -f util/verilator-openocd-cfg.tcl &
-sleep 20
-util/gdb_response.exp
+sleep 5
+ls build/sw/device/examples
+gdb --batch -ex "quit" build/sw/device/examples/infinite_loop
+echo "exit: $?"
+#expect util/gdb_response.exp
 
 # The pkill command is not available in GitHub actions.
 #pkill openocd
